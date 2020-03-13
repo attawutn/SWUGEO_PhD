5587a3515b52cd3c8f69ae28671bfcc6cef00f8c
Attawut Nardkulpat <earmla.n@hotmail.co.uk>
2020-03-13 23:32:19 +0700

    Update xgboost.R

--- xgboost.R
+++ xgboost.R
@@ -2,7 +2,7 @@ library(raster)
 library(xgboost)
 
 shp <- shapefile("XGboost/Ri62_Suphanburi_Intersect_po.shp")
-ras <- stack("XGboost/subset_0_of_S1B_IW_GRDH_1SDV_20200210T230834_20200210T230859_020213_02645C_DCDF_TNR_Orb_Cal_Spk_TC.tif")
+ras <- stack("XGboost/test_data.tif")
 vals <- extract(ras,shp)
 
 train <- data.matrix(vals)
@@ -20,4 +20,4 @@ result <- predict(xgb,ras[1:(nrow(ras)*ncol(ras))])
 res <- raster(ras)
 res1 <- setValues(res,result + 1)
 
-writeRaster(res1,'test1.tif',options=c('TFW=YES'))
+writeRaster(res1,'test2.tif',options=c('TFW=YES'))
