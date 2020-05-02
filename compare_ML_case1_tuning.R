76444641dd049383421d18483e4ad1337711e51e
Attawut Nardkulpat <earmla.n@hotmail.co.uk>
2020-05-02 21:30:51 +0700

    update

--- compare_ML_case1_tuning.R
+++ compare_ML_case1_tuning.R
@@ -50,39 +50,46 @@ for(i in 1:n_folds) seeds[[i]] <- sample.int(1000, n_folds)
 seeds[n_folds + 1] <- sample.int(1000, 1) # seed for the final model
 
 ctrl <- trainControl(summaryFunction = multiClassSummary,
-                     method = "cv",
-                     number = 5,
+                     method = "repeatedcv",
+                     number = 10,
+                     repeats = 5,
                      search = "grid",
-                     classProbs = TRUE, # not implemented for SVM; will just get a warning
-                     savePredictions = 'final')
+                     classProbs = TRUE,
+                     savePredictions = 'final',
+                     allowParallel = TRUE,
+                     verboseIter = TRUE)
+
+nnet.grid = expand.grid(size = seq(from = 2, to = 10, by = 2), # number of neurons units in the hidden layer 
+                        decay = seq(from = 0.1, to = 0.5, by = 0.1)) # regularization parameter to avoid over-fitting 
+
+rf.grid <- expand.grid(mtry=1:20) # number of variables available for splitting at each tree node
+
+svm.grid <- expand.grid(sigma=seq(from = 0.01, to = 0.10, by = 0.02), # controls for non-linearity in the hyperplane
+                        C=seq(from = 2, to = 10, by = 2)) # controls the influence of each support vector
 
 
 # Train the model using Random Forest
-model_rf <- train(id_cls ~ .  ,data = dt_train, method = "rf",importance = TRUE,trControl = ctrl)
-
-model_rf
-plot(model_rf)
+model_rf <- train(id_cls ~ .  ,data = dt_train, method = "rf",metric="Accuracy",importance = TRUE,trControl = ctrl,tuneGrid = rf.grid)
+#model_rf
+#plot(model_rf)
 
 cm_rf <- confusionMatrix(data = predict(model_rf, newdata = dt_test),
                          dt_test$id_cls)
 
 # Train the model using SVM:Ridial Basis
-model_svmRadial = train(id_cls ~ .  ,data = dt_train, method='svmRadialCost', tuneLength=5, trControl = ctrl)
+model_svmRadial = train(id_cls ~ .  ,data = dt_train, method='svmRadialSigma',metric="Accuracy", tuneGrid = svm.grid, trControl = ctrl)
 
-model_svmRadial
-plot(model_svmRadial)
+#model_svmRadial
+#plot(model_svmRadial)
 
 cm_svm <- confusionMatrix(data = predict(model_svmRadial, newdata = dt_test),dt_test$id_cls)
-cm_svm
+#cm_svm
 
 
 #Train Neural Network
-model_nn <- train(id_cls ~ .  ,data = dt_train, 
-               method = "nnet", trControl = ctrl,
-               linout = TRUE,)
-cm_nn <- confusionMatrix(data = predict(model_nn, newdata = dt_test),
-                          dt_test$id_cls)
-cm_nn
+model_nn <- train(id_cls ~ .  ,data = dt_train,method = "nnet", trControl = ctrl,metric="Accuracy",linout = TRUE,tuneGrid = nnet.grid)
+cm_nn <- confusionMatrix(data = predict(model_nn, newdata = dt_test),dt_test$id_cls)
+#cm_nn
 
 
 # Train the model usingXGBoost
@@ -96,22 +103,22 @@ xgb_trcontrol = trainControl(
 )
 
 xgbGrid <- expand.grid(nrounds = c(100,200),
-                  max_depth = c(10, 15, 20, 25),
-                  colsample_bytree = seq(0.5, 0.9, length.out = 5),
-                  eta = 0.1,
-                  gamma=0,
-                  min_child_weight = 1,
-                  subsample = 1
+                       max_depth = c(10, 15, 20, 25),
+                       colsample_bytree = seq(0.5, 0.9, length.out = 5),
+                       eta = 0.1,
+                       gamma=0,
+                       min_child_weight = 1,
+                       subsample = 1
 )
 set.seed(100) 
 xgb_model = train(id_cls ~ .  ,data = dt_train,  
-  trControl = ctrl,
-  tuneGrid = xgbGrid,
-  method = "xgbTree"
+                  trControl = ctrl,
+                  tuneGrid = xgbGrid,
+                  method = "xgbTree"
 )
 
 cm_xgb <- confusionMatrix(data = predict(xgb_model, newdata = dt_test),
-                         dt_test$id_cls)
+                          dt_test$id_cls)
 cm_xgb
 
 
@@ -119,16 +126,16 @@ cm_xgb
 models_compare <- resamples(list(RF=model_rf, SVM=model_svmRadial,NN=model_nn,xgb=xgb_model))
 summary(models_compare)
 
-###Predict raster
-
+##Predict
 nnet_prediction = predict(ras, model=model_nn)
-writeRaster(nnet_prediction,'test_nn_untune.tif',options=c('TFW=YES'))
+writeRaster(nnet_prediction,'test_nn.tif',options=c('TFW=YES'))
 
 rf_prediction = predict(ras, model=model_rf)
-writeRaster(rf_prediction,'test_rf_untune.tif',options=c('TFW=YES'))
+writeRaster(rf_prediction,'test_rf.tif',options=c('TFW=YES'))
 
 svm_prediction = predict(ras, model=model_svmRadial)
-writeRaster(svm_prediction,'test_svm_untune.tif',options=c('TFW=YES'))
+writeRaster(svm_prediction,'test_svm.tif',options=c('TFW=YES'))
 
 xgb_prediction = predict(ras, model=xgb_model)
-writeRaster(xgb_prediction,'test_xgb_untune.tif',options=c('TFW=YES'))
+writeRaster(xgb_prediction,'test_xgb.tif',options=c('TFW=YES'))
+
